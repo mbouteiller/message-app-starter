f5745596e676add5613f9d63aa171fbce1203769
Pourtier <remi.pourtier@accenture.com>
2020-10-11 13:29:12 +0200

    Project initialization

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="app">
+    <HelloWorld :msg="'Welcome to Your Vue.JS App'"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  }
+};
+</script> 
+
+<style>
+html, body {
+  height: 100%;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
